c32714cfc275f332082f3030ef9e4558faa8df74
Viet Le cuongle1998@hotmail.com <cuongle1998@hotmail.com>
2023-02-20 12:53:45 +1100

    added task model and list function

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,6 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  get '/index', to: 'tasks#index'
+  get 'tasks/:id', to: 'tasks#show', as: 'task'
 end
