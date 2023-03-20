776171742600ff990b36599cd4929569e78daf26
Viet Le cuongle1998@hotmail.com <cuongle1998@hotmail.com>
2023-03-20 21:27:11 +1100

    removed comments and added root path

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -6,4 +6,30 @@ class TasksController < ApplicationController
   def show
     @task = Task.find(params[:id])
   end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(task_params)
+    @task.save
+    redirect_to tasks_path
+  end
+
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    @task = Task.find(params[:id])
+    @task.update(task_params)
+    redirect_to task_path(@task)
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:title, :details, :completed)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,18 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
-  get '/index', to: 'tasks#index'
+  root 'tasks#index'
+  # Read all tasks
+  get '/tasks', to: 'tasks#index'
+
+  # Create new task
+  get 'tasks/new', to: 'tasks#new'
+  post 'tasks', to: 'tasks#create'
+
+  # Show individual task
   get 'tasks/:id', to: 'tasks#show', as: 'task'
+
+  # Edit task
+  get 'tasks/:id/edit', to: 'tasks#edit', as: 'task_edit'
+  patch 'tasks/:id', to: 'tasks#update'
 end
